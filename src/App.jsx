aa4b5ebec104e384e8f84293c0d9145b9fc5fb56
Mateus Machado <matheus.ffm@educacao.brusque.sc.gov.br>
2026-03-27 15:00:09 -0300

    Ajusta rotas para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import AgendaPage from "./pages/AgendaPage";
 import CadastrosPage from "./pages/CadastrosPage";
@@ -9,19 +9,18 @@ import RelatoriosPage from "./pages/RelatoriosPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Navigate to="/agenda" replace />} />
-          <Route path="/agenda" element={<AgendaPage />} />
-          <Route path="/cadastros" element={<CadastrosPage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/estoque" element={<EstoquePage />} />
-          <Route path="/vendas" element={<VendasPage />} />
-          <Route path="/relatorios" element={<RelatoriosPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Navigate to="/agenda" replace />} />
+        <Route path="agenda" element={<AgendaPage />} />
+        <Route path="cadastros" element={<CadastrosPage />} />
+        <Route path="menu" element={<MenuPage />} />
+        <Route path="estoque" element={<EstoquePage />} />
+        <Route path="vendas" element={<VendasPage />} />
+        <Route path="relatorios" element={<RelatoriosPage />} />
+        <Route path="*" element={<Navigate to="/agenda" replace />} />
+      </Route>
+    </Routes>
   );
 }
 
